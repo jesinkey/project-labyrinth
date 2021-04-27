aacccc18d4f01279cfdc43ca5a8c75ea922b0c4d
matilda svensson <info@matildasvensson.se>
2021-04-27 18:07:01 +0200

    working on second fetch, now it works

--- code/src/components/StartPage.js
+++ code/src/components/StartPage.js
@@ -2,14 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux' 
 import { fetchStart } from 'reducers/game'
+import { fetchNext } from 'reducers/game'
+import { game } from 'reducers/game'
 import { useSelector } from 'react-redux'
 
 export const StartPage = () => {
     const dispatch = useDispatch()
 
-    const descriptionText = useSelector((store)=>store.game.description)
+    const description = useSelector((store)=>store.game.description)
     const coordinates = useSelector((store)=>store.game.coordinates)
-    const actionsObject = useSelector((store) => store.game.actions)
+    const actions = useSelector((store) => store.game.actions)
+    const actionDirection = actions.direction
+    const actionType = actions.type
+    console.log(actionType)
+    console.log(actionDirection)
 
     return (
         <>
@@ -21,13 +27,26 @@ export const StartPage = () => {
                 </button>
             {/* </Link> */}
 
+            <h2>{description}</h2>
+            <p>Your coordinates: {coordinates}</p>
+            <p>Description: {actions.description}</p>
+            <p>Direction: {actionDirection}</p>
+            <p>Type: {actions.type}</p>
 
-            <h2>{descriptionText}</h2>
-        <p>{coordinates}</p>
-        <p>{actionsObject.description}</p>
-               
-               
+            <button
+                // onClick={() => dispatch(fetchNext({ type: actionType, direction: actionDirection }))}>
+                // HÄR SKA DE IN
+                onClick={() => dispatch(fetchNext(actionDirection, actionType))}>
+                Go {actionDirection}
+            </button>
 
+            {/* {actionsObject.map(action => (
+                <div key={actions.direction}>
+                    <p>{actions.description}</p>
+                    <p>{actions.direction}</p>
+                    <p>{actions.type}</p>
+                </div>
+            ))} */}
 
         </>
     )
--- code/src/reducers/game.js
+++ code/src/reducers/game.js
@@ -7,28 +7,30 @@ const fetchInitStart = {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ username: 'MaHal1' }),
+    body: JSON.stringify({ username: 'Matilda000' }),
   };
-
-//   const fetchInitNext = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({ 
-//         username: 'MaHal1',
-//         type: '',
-//         direction: ''
-//     }),
-//   };
+// HÄR SKA DE IN + DEN ÄR FÖRVANDLAD TILL ARROWWFUNCTION MED RETURN
+  const fetchInitNext = (actionDirection, actionType) => {
+    return {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+            username: 'Matilda000',
+            direction: actionDirection,
+            type: actionType
+        }),
+    }
+  };
 
 export const game = createSlice({
     name: 'game',
     initialState: {
         description: '',
         actions: {
-            type: '',
-            direction: '',
+            type: null,
+            direction: null,
             description: ''
         },
         coordinates: ''
@@ -44,7 +46,12 @@ export const game = createSlice({
         },
         setActions: (store, action) => {
             store.actions = action.payload
-            console.log(store.actions)
+
+            // console.log(`Action: ${store.actions}`)
+            // console.log(store.actions)
+            // console.log(store.actions.description)
+            // console.log(store.actions.type)
+            // console.log(store.actions.direction)
         }
     }
 })
@@ -63,17 +70,23 @@ export const fetchStart = () => {
             })
     }
 }
-
-// export const fetchNext = () => {
-//     return (dispatch, getState) => {
-//         fetch('https://wk16-backend.herokuapp.com/action', fetchInitNext)
-//             .then(res => res.json())
-//             .then(json => {
-//                 console.log(json)
-//                 dispatch(game.actions.setDescription(json.description))
-//                 dispatch(game.actions.setCoordinates(json.coordinates))
-//                 dispatch(game.actions.setActions(json.actions[0]))
-//             })
-//     }
-// }
+// HÄR SKA DE IN
+export const fetchNext = (actionDirection, actionType) => {
+    // const { actionType, actionDirection } = action.payload
+    console.log('hello')
+    console.log(actionType)
+    console.log(actionDirection) 
+    return (dispatch, getState) => {
+        fetch('https://wk16-backend.herokuapp.com/action', 
+        // HÄR SKA DE IN
+        fetchInitNext(actionDirection, actionType))
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                dispatch(game.actions.setDescription(json.description))
+                dispatch(game.actions.setCoordinates(json.coordinates))
+                dispatch(game.actions.setActions(json.actions))
+            })
+    }
+}
 
